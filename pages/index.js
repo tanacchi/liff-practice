1d06507534e0f7b1a75975eafe0819a5e1144537
tanacchi <q111026d@mail.kyutech.jp>
2025-04-20 19:38:32 +0900

    Use liff objec

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,30 @@ import packageJson from "../package.json";
 
 export default function Home(props) {
   /** You can access to liff and liffError object through the props.
-   *  const { liff, liffError } = props;
-   *  console.log(liff.getVersion());
-   *
    *  Learn more about LIFF API documentation (https://developers.line.biz/en/reference/liff)
    **/
+  const { liff, liffError } = props;
+  console.log(liff.getVersion());
+  const profile = liff.getProfile();
+
   return (
     <div>
       <Head>
         <title>LIFF practice</title>
       </Head>
-      <div className="home">
-        こんにちは
-      </div>
+      <div className="home">こんにちは</div>
+      <div className="home">LIFF Version: {liff.getVersion()}</div>
+      <div className="home">LIFF Error: {liffError}</div>
+      <div className="home">LIFF ID: {liff.getContext().liffId}</div>
+      <button onClick={() => liff.sendMessages([{
+        type: "text",
+        text: "Hello World",
+      }])}>
+        Send Message
+      </button>
+      <button onClick={() => liff.closeWindow()}>
+        Close Window
+      </button>
     </div>
   );
 }
